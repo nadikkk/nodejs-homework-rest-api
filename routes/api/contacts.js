abfe499cdf1d8d12a54e1fafa5cefc7dd4765690
nadikkk <109106413+nadikkk@users.noreply.github.com>
2023-06-20 15:15:18 +0300

    ignore node

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const ctrl = require("../../controllers");
-const validateBody = require("../../middlewares");
-const addSchema = require("../../schemas");
-const router = express.Router();
+const express = require('express')
+const ctrl = require("../../controllers")
+const validateBody = require('../../middlewares')
+const addSchema = require('../../schemas')
+const router = express.Router()
 
-router.get("/", ctrl.getAll);
+router.get('/', ctrl.getAll)
 
-router.get("/:contactId", ctrl.getById);
+router.get('/:contactId', ctrl.getById)
 
-router.post("/", validateBody(addSchema), ctrl.add);
+router.post('/', validateBody(addSchema), ctrl.add)
 
-router.delete("/:contactId", ctrl.remove);
+router.delete('/:contactId', ctrl.remove)
 
-router.put("/:contactId", validateBody(addSchema), ctrl.update);
+router.put('/:contactId', validateBody(addSchema), ctrl.update)
 
-module.exports = router;
+module.exports = router
